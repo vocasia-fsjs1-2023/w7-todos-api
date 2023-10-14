1e2ca5f69aa15bafcfd96fd98f91570dd4f60822
evaald <evaaldori239@gmail.com>
2023-10-14 23:48:42 +0700

    Problem 7 Todos API Eva

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 const port = 3000;
 
 const ToDoList = [];
