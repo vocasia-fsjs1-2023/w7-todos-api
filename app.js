3010e5253a8c9eb37282a977b3f60bd8a778b4e1
johan <johan992001@gmail.com>
2023-10-18 05:59:01 +0800

    pengumpuan tugas

---
+++ app.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const app = express();
+
+app.use(express.text());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
+
+const port = 3000;
+
+const todos = [];
+
+let id = 1;
+
+const getFormattedDate = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+};
+
+app.post("/todos", (req, res) => {
+    const { title, description } = req.body;
+    const newTodo = {
+        id: id++,
+        title,
+        description,
+        status: 'created',
+        created_at: getFormattedDate(),
+        updated_at: getFormattedDate()
+    };
+
+    todos.push(newTodo);
+
+    res.status(201).json(newTodo);
+});
+
+app.get("/todos", (req, res) => {
+    res.status(200).json(todos);
+});
+
+app.put("/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const { title, description } = req.body;
+
+    const todoToUpdate = todos.find((todo) => todo.id === parseInt(id));
+    if (todoToUpdate) {
+        todoToUpdate.title = title;
+        todoToUpdate.description = description;
+        todoToUpdate.updated_at = getFormattedDate();
+
+        res.status(200).json(todoToUpdate);
+    } else {
+        res.status(404).json({ message: "Tidak terdeteksi" });
+    }
+});
+
+app.patch("/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const { status } = req.body;
+
+    const todoToUpdate = todos.find((todo) => todo.id === parseInt(id));
+    if (todoToUpdate) {
+        todoToUpdate.status = status;
+        todoToUpdate.updated_at = getFormattedDate();
+        res.status(200).json(todoToUpdate);
+    } else {
+        res.status(404).json({ message: "Tidak terdeteksi" });
+    }
+});
+
+app.delete("/todos/:id", (req, res) => {
+    const { id } = req.params;
+
+    const index = todos.findIndex((todo) => todo.id === parseInt(id));
+    if (index !== -1) {
+        const deletedTodo = todos.splice(index, 1)[0];
+        res.status(200).json({ message: `Todo dengan ID ${id} berhasil dihapus`, todo: deletedTodo });
+    } else {
+        res.status(404).json({ message: "Tidak terdeteksi" });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Berjalan di port ${port}`);
+});
